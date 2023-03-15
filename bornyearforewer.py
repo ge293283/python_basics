c4bd05f072c6c41f304d8127142fb1c1537816de
ge293283 <ge293283@gmail.com>
2023-03-15 22:02:29 +0400

    0.0

--- bornyearforewer.py
+++ bornyearforewer.py
@@ -1,11 +1,13 @@
 # Module 4
 
+# А.С. Пушкин
 yearOfBirth_Pushkin = input("Введите год рождения А.С. Пушкина (например: 1900): ")
 while yearOfBirth_Pushkin != "1799":
     print("Неверно")
     yearOfBirth_Pushkin = input("Введите год рождения А.С. Пушкина (например: 1900): ")
 print("Верно\n")
 
+# М.А. Булгаков
 while True:
     yearOfBirth_Bulgakov = input("Введите год рождения М.А. Булгакова (например: 1900): ")
     if yearOfBirth_Bulgakov == "1891":
