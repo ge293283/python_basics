3d3a9d6b600498ec6c6c9f861650f2052b2bb6f2
ge293283 <ge293283@gmail.com>
2023-03-15 22:30:41 +0400

    0.0

--- borndayforewer.py
+++ borndayforewer.py
@@ -1,5 +1,6 @@
 # Module 5
 
+# We ask for the year of birth and check until the correct answer is entered, if ok - go to the next cycle
 while True:
     yearOfBirth = input("Введите год рождения А.С. Пушкина (например: 1900): ")
     if yearOfBirth == "1799":
@@ -8,9 +9,10 @@ while True:
     else:
         print("Неверный год рождения")
 
+# We request and check the date of birth, if ok - exit the loop and end the program
 while True:
     dayOfBirth = input("Введите день рождения А.С. Пушкина (например: 01.01): ")
-    if dayOfBirth == "26.05" or dayOfBirth == "06.06":
+    if dayOfBirth == "26.05" or dayOfBirth == "06.06": # The date can be entered according to the old style or the new one
         break
     else:
         print("Неверный день рождения")
