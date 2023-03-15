f310ed933bf69d195275e6c6170bd203146eabc4
ge293283 <ge293283@gmail.com>
2023-03-15 21:17:58 +0400

    0.0

---
+++ variables.py
@@ -0,0 +1,29 @@
+# Module 1
+
+# Имя сотрудкика (str)
+name_employee = "John Smith"
+print(type(name_employee))
+
+# Возраст сотрудника (int)
+age_employee = 19
+print(type(age_employee))
+
+# Ставка (float)
+wageRate_employee = 0.5
+print(type(wageRate_employee))
+
+# Находится ли в отпуске или нет (bool)
+is_onLeave_employee = False
+print(type(is_onLeave_employee))
+
+# Список задач (list)
+task_list_employee = ["Отпуск",]
+print(type(task_list_employee))
+
+# Используемые кабинеты (set)
+ued_cabinets_employee = {'334', 'Зал совещаний', 'Переговорная комната № 4', 'Кухня'}
+print(type(ued_cabinets_employee))
+
+# Контакты (dict)
+contacts_employee = {"Amelia Parker": "22-11", "Harper Foster": "22-06", "Madison Scott": "22-05"}
+print(type(contacts_employee))
