e4b83fd5d52a72f982302f2b819f984d333095a8
ge293283 <ge293283@gmail.com>
2023-03-15 22:22:20 +0400

    0.0

---
+++ borndayforewer.py
@@ -0,0 +1,15 @@
+# Module 5
+
+while True:
+    yearOfBirth = input("Введите год рождения А.С. Пушкина (например: 1900): ")
+    if yearOfBirth == "1799":
+        break
+    else:
+        print("Неверный год рождения")
+
+while True:
+    dayOfBirth = input("Введите день рождения А.С. Пушкина (например: 01.01): ")
+    if dayOfBirth == "26.05":
+        break
+    else:
+        print("Неверный день рождения")
